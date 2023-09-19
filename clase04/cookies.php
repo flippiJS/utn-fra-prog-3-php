93ea8623519c0b033f6a9622f685c6a4283e391b
Franco Lippi <soyflippi@gmail.com>
2023-09-18 21:37:15 -0300

    Subida clase 03 y 04

--- clase04/cookies.php
+++ clase04/cookies.php
@@ -1,7 +1,9 @@
 <?php
+    // Ejemplo Cookies
 
     if( isset( $_COOKIE['prueba']) )
     {
+        // Verificammos que esta creada la cookie "prueba"
         echo "<p>La cookie está creada</p>";
     }
     else

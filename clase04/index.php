7a8f228d7c51e1db7b2212b16a84fb5c3595aa75
Franco Lippi <soyflippi@gmail.com>
2023-09-19 19:11:19 -0300

    Subida clase 03 y 04

--- clase04/index.php
+++ clase04/index.php
@@ -1,35 +1,39 @@
 <?php
-
-switch($_SERVER['REQUEST_METHOD']){
-    case 'GET':
-        switch ($_GET['accion']){
-            case 'sesion':
-                include 'sesiones.php';
-                break;
-            case 'cookies':
-                include 'cookies.php';
-                break;
-            case 'json':
-                include 'json.php';
-                break;
-            default:
-                echo 'Parámetro "accion" no permitido';
-                break;
-        }
-        break;
-    case 'POST':
-        switch ($_GET['accion']){
-            case 'archivo':
-                include 'archivos.php';
-                break;
-            default:
-                echo 'Parámetro "accion" no permitido';
-                break;
-        }
-        break;
-    default:
-        echo 'Verbo no permitido';
-        break;
+// Verificamos que el parámetro accion este definido
+if(isset($_GET['accion'])){
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'GET':
+            switch ($_GET['accion']){
+                case 'sesion':
+                    include 'sesiones.php';
+                    break;
+                case 'cookies':
+                    include 'cookies.php';
+                    break;
+                case 'json':
+                    include 'json.php';
+                    break;
+                default:
+                    echo 'Parámetro "accion" no permitido';
+                    break;
+            }
+            break;
+        case 'POST':
+            switch ($_GET['accion']){
+                case 'archivo':
+                    include 'archivos.php';
+                    break;
+                default:
+                    echo 'Parámetro "accion" no permitido';
+                    break;
+            }
+            break;
+        default:
+            echo 'Verbo no permitido';
+            break;
+    }
+} else {
+    echo 'Parámetro "accion" no enviado';
 }
 
 ?>
